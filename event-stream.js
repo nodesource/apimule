f36df5a1e71310e3a0d7d06e71cda446b40d0c50
Patrick Mueller <pmuellr@nodesource.com>
2016-12-01 07:56:45 -0500

    undo previous commit, which was made in error

--- event-stream.js
+++ event-stream.js
@@ -8,22 +8,21 @@ const ContentTypeTextHTML = 'text/html'
 const ContentTypeJSONStream = 'application/x-json-stream'
 const ContentTypeEventStream = 'text/event-stream'
 
-const Noop = function () {}
-
 // Create a new event stream for an HTTP request.
-function create (context, req, res) {
-  return new EventStream(context, req, res)
+function create (context, logger, req, res) {
+  return new EventStream(context, logger, req, res)
 }
 
 // Handles writing to, closing, detecting closure of event stream.
-// All errback callbacks pass no data on success, just errors when they occur.
 class EventStream {
 
-  constructor (context, req, res) {
+  constructor (context, logger, req, res) {
+    this._context = context
     this._req = req
     this._res = res
     this._isClosed = false
     this._isEventStream = false
+    this.Logger = logger
 
     // accept headers:
     // * chrome:  text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'
@@ -54,34 +53,24 @@ class EventStream {
     this._res.on('close', () => this.close())
   }
 
-  // Returns indication of whether the stream is closed or not.
-  isClosed () {
-    return this._isClosed
-  }
+  // write a heartbeat comment
+  writeHeartBeat () {
+    if (this._isClosed) return false
 
-  // Write an event to the event stream.
-  // The callback is invoked as cb(err, null).
-  writeData (body, cb) {
-    if (cb == null) cb = Noop
+    if (!this._isEventStream) return true
 
-    if (body == null) return setImmediate(cb, new Error('body was null'))
-
-    if (this.isClosed()) return setImmediate(cb)
-
-    const event = lodash.defaults({}, body)
-
-    this._res.write(this._frameLine(JSON.stringify(event)), 'utf8', cb)
+    this._res.write(': heart beat\n')
+    return true
   }
 
   // Write an error to the event stream.
   // Returns true if the error was written, false if stream was closed.
-  writeError (name, message, errorProps, cb) {
-    if (name == null) name = 'error'
-    if (message == null) message = 'an error occurred'
-    if (errorProps == null) errorProps = {}
-    if (cb == null) cb = Noop
+  writeError (id, name, message, errorProps) {
+    if (this._isClosed) return false
 
     const event = {}
+
+    if (id) event.id = id
 
     event.error = {
       name: name,
@@ -94,65 +83,74 @@ class EventStream {
       }
     }
 
-    this.writeData(event, cb)
+    this._res.write(this.frameLine(JSON.stringify(event)))
+    return true
   }
 
-  // Write a comment
-  writeComment (comment, cb) {
-    if (comment == null) comment = 'no comment'
-    if (cb == null) cb = Noop
+  // write 2K worth of filler comment, for polyfills
+  writeFiller () {
+    if (this._isClosed) return false
+    if (!this._isEventStream) return true
 
-    if (this.isClosed()) return setImmediate(cb)
-    if (!this._isEventStream) return setImmediate(cb)
-
-    this._res.write(`: ${comment}\n`, 'utf8', cb)
+    this._res.write(`: ${new Array(4096).join(' ')}\n`)
+    return true
   }
 
-  // Write a heartbeat comment
-  writeHeartBeat (cb) {
-    writeComment('heart beat', cb)
-  }
+  // write an event to the event stream
+  // Returns true if the event was written, false if stream was closed.
+  writeData (id, body) {
+    if (body == null) {
+      body = id
+      id = null
+    }
+    if (this._isClosed) return false
 
-  // Write 2K worth of filler comment, for polyfills
-  writeFiller (cb) {
-    writeComment(new Array(4096).join(' '), cb)
+    const event = lodash.defaults({}, body)
+
+    if (id) event.id = id
+
+    try {
+      this._res.write(this.frameLine(JSON.stringify(event)))
+    } catch (err) {
+      this.Logger.error('Could not write to event stream.', err, this)
+    }
+    return true
   }
 
   // Close the event stream.
-  close (cb) {
-    if (cb == null) cb = Noop
-
-    if (this.isClosed()) return setImmediate(cb)
+  close () {
+    if (this._isClosed) return false
+    if (this._res == null) return
 
     if (this._contentType === ContentTypeEventStream) {
-      this.writeData({end: true}, sentEnd)
-    } else {
-      setImmediate(() => sentEnd())
+      this._res.write(this.frameLine('{"end": true}'))
     }
 
-    const self = this
-
-    // sent end packet
-    function sentEnd (err) {
-      if (err) return cb(err)
-
-      if (self._res == null) return setImmediate(cb)
-
-      self._res.end(cb)
-
-      self._req = null
-      self._res = null
-      self._isClosed = true
+    try {
+      this._res.end()
+    } catch (err) {
+      this.Logger.error(err, 'error closing event-stream socket')
     }
+
+    clearInterval(this.heartBeatInterval)
+
+    this._isClosed = true
+    this._context = null
+    this._req = null
+    this._res = null
+    this.heartBeatInterval = true
   }
 
-  // Write a line of data, formatted as appropriate for the type of stream.
-  _frameLine (line) {
+  frameLine (line) {
     if (this._isEventStream) {
       return `data: ${line}\n\n`
     } else {
       return `${line}\n`
     }
+  }
+  // Returns indication of whether the stream is closed or not.
+  isClosed () {
+    return this._isClosed
   }
 
 }
